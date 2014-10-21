7f87ceef5424f3f0aa7ce40450130944871c95ea
rocalabern <rocalabern@gmail.com>
2014-10-21 10:27:28 +0200

    .

--- R/rleaflet.R
+++ R/rleaflet.R
@@ -5,6 +5,51 @@ double.escape<-function(textvector) {
   return(text.out)
 }
 
+getLabel <- function (array, strLabel) {
+  if (is.null(array)) return (NULL)
+  else return (strLabel)
+}
+
+getAttributeValue <- function (label, array=NULL, constant=NULL) {
+  if (!is.null(array)) return (paste0("feature.properties.",label))
+  else if (!is.null(constant)) {
+    if (is.logical(constant)) {
+      if (constant) return ("true")
+      else return ("false")
+    } else return (constant)
+  } else return (NULL)
+}
+
+addAttribute <- function (strAttributesList, label, array=NULL, constant=NULL) {
+  strNewAttributeValue = getAttributeValue(label, array, constant)
+  
+  if (is.null(strNewAttributeValue)) return (strAttributesList)
+  else if (is.null(strAttributesList)) paste0(label,":",strNewAttributeValue)
+  else paste0(strAttributesList,", ",label,":",strNewAttributeValue)
+}
+
+#' r.addByWord
+#' @export
+r.addByWord <- function(listText, strWord, ind=NULL, ignore.case=TRUE) {
+  if (is.null(ind) || length(ind)==0) {
+    if (length(strWord)==0) return (NULL)
+    if (length(strWord)==1) return (which(grepl(strWord, listText, ignore.case=ignore.case)))
+    else  return (union(which(grepl(strWord[1], listText, ignore.case=ignore.case)),r.addByWord(listText, strWord[2:length(strWord)])))
+  } else {
+    if (length(strWord)==0) return (ind)
+    if (length(strWord)==1) return (union(ind,which(grepl(strWord, listText, ignore.case=ignore.case))))
+    else  return (union(ind,union(which(grepl(strWord[1], listText, ignore.case=ignore.case)),r.addByWord(listText, strWord[2:length(strWord)]))))
+  }
+}
+
+#' r.map.getProviderList
+#' @export
+r.map.getProviderList <- function(type=NULL) {
+  if (is.null(type)) return (c("OpenStreetMap.Mapnik","OpenStreetMap.BlackAndWhite","OpenStreetMap.DE","OpenStreetMap.HOT","Thunderforest.OpenCycleMap","Thunderforest.Transport","Thunderforest.Landscape","Thunderforest.Outdoors","OpenMapSurfer.Roads","OpenMapSurfer.Grayscale","Hydda.Full","Hydda.Base","MapQuestOpen.OSM","MapQuestOpen.Aerial","Stamen.Toner","Stamen.TonerBackground","Stamen.TonerLite","Stamen.Terrain","Stamen.TerrainBackground","Stamen.Watercolor","Esri.WorldStreetMap","Esri.DeLorme","Esri.WorldTopoMap","Esri.WorldImagery","Esri.WorldTerrain","Esri.WorldShadedRelief","Esri.WorldPhysical","Esri.OceanBasemap","Esri.NatGeoWorldMap","Esri.WorldGrayCanvas","HERE.normalDay","HERE.normalDayCustom","HERE.normalDayGrey","HERE.normalDayMobile","HERE.normalDayGreyMobile","HERE.normalDayTransit","HERE.normalDayTransitMobile","HERE.normalNight","HERE.normalNightMobile","HERE.normalNightGrey","HERE.normalNightGreyMobile","HERE.carnavDayGrey","HERE.hybridDay","HERE.hybridDayMobile","HERE.pedestrianDay","HERE.pedestrianNight","HERE.satelliteDay","HERE.terrainDay","HERE.terrainDayMobile","Acetate.basemap","Acetate.terrain","Acetate.all","Acetate.hillshading","FreeMapSK","MtbMap","OpenMapSurfer.AdminBounds","Hydda.RoadsAndLabels","Stamen.TonerHybrid","Stamen.TonerLines","Stamen.TonerLabels","OpenWeatherMap.Clouds","OpenWeatherMap.CloudsClassic","OpenWeatherMap.Precipitation","OpenWeatherMap.PrecipitationClassic","OpenWeatherMap.Rain","OpenWeatherMap.RainClassic","OpenWeatherMap.Pressure","OpenWeatherMap.PressureContour","OpenWeatherMap.Wind","OpenWeatherMap.Temperature","OpenWeatherMap.Snow","Acetate.foreground","Acetate.roads","Acetate.labels"))
+  else if (tolower(type)=="favorite") return (c("OpenStreetMap.BlackAndWhite","OpenStreetMap.DE","Hydda.Full","Hydda.Base","Stamen.Toner","Stamen.Watercolor","Esri.WorldStreetMap","Esri.DeLorme","Esri.WorldTopoMap","Stamen.TonerHybrid","Stamen.TonerLines","HERE.terrainDay","HERE.normalDayGrey","HERE.normalNightGrey","HERE.pedestrianNight","HERE.satelliteDay","Acetate.terrain","Acetate.all","MtbMap"))
+  return (c("OpenStreetMap.Mapnik","OpenStreetMap.BlackAndWhite","OpenStreetMap.DE","OpenStreetMap.HOT","Thunderforest.OpenCycleMap","Thunderforest.Transport","Thunderforest.Landscape","Thunderforest.Outdoors","OpenMapSurfer.Roads","OpenMapSurfer.Grayscale","Hydda.Full","Hydda.Base","MapQuestOpen.OSM","MapQuestOpen.Aerial","Stamen.Toner","Stamen.TonerBackground","Stamen.TonerLite","Stamen.Terrain","Stamen.TerrainBackground","Stamen.Watercolor","Esri.WorldStreetMap","Esri.DeLorme","Esri.WorldTopoMap","Esri.WorldImagery","Esri.WorldTerrain","Esri.WorldShadedRelief","Esri.WorldPhysical","Esri.OceanBasemap","Esri.NatGeoWorldMap","Esri.WorldGrayCanvas","HERE.normalDay","HERE.normalDayCustom","HERE.normalDayGrey","HERE.normalDayMobile","HERE.normalDayGreyMobile","HERE.normalDayTransit","HERE.normalDayTransitMobile","HERE.normalNight","HERE.normalNightMobile","HERE.normalNightGrey","HERE.normalNightGreyMobile","HERE.carnavDayGrey","HERE.hybridDay","HERE.hybridDayMobile","HERE.pedestrianDay","HERE.pedestrianNight","HERE.satelliteDay","HERE.terrainDay","HERE.terrainDayMobile","Acetate.basemap","Acetate.terrain","Acetate.all","Acetate.hillshading","FreeMapSK","MtbMap","OpenMapSurfer.AdminBounds","Hydda.RoadsAndLabels","Stamen.TonerHybrid","Stamen.TonerLines","Stamen.TonerLabels","OpenWeatherMap.Clouds","OpenWeatherMap.CloudsClassic","OpenWeatherMap.Precipitation","OpenWeatherMap.PrecipitationClassic","OpenWeatherMap.Rain","OpenWeatherMap.RainClassic","OpenWeatherMap.Pressure","OpenWeatherMap.PressureContour","OpenWeatherMap.Wind","OpenWeatherMap.Temperature","OpenWeatherMap.Snow","Acetate.foreground","Acetate.roads","Acetate.labels"))
+}
+
 #' r.leaflet
 #' @export
 r.leaflet<-function(
@@ -111,4 +156,125 @@ r.leaflet<-function(
   if (openmap) {
     browseURL(filename)
   }
+}
+
+#' r.map.new
+#' @export
+r.map.new <- function(
+  mapCenter = c(41.39,  2.16), 
+  mapZoom = 13,
+  mapTiles = "MapQuestOpen.OSM",
+  enablePopover = FALSE,
+  fullScreen = TRUE) {
+  m <- Leaflet$new()
+  m$setView(mapCenter, mapZoom)
+  m$tileLayer(provider = mapTiles)
+  m$enablePopover(enablePopover)
+  m$fullScreen(fullScreen)  
+  return (m)
+}
+
+#' r.map.addHeatMap
+#' @export
+r.map.addHeatMap <- function (m, lat, lon) {
+  posJSON = toJSONArray2(cbind(lat, lon), json = F, names = F)
+  m$addAssets(jshead = c("http://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js"))
+  m$setTemplate(afterScript = sprintf(
+    "
+    <script>
+    var addressPoints = %s;
+    var heat = L.heatLayer(addressPoints).addTo(map);
+    </script>
+    ", rjson::toJSON(posJSON)
+  ))
+  return (m)
+}
+
+#' r.map.addGeoJSON
+#' @export
+r.map.addGeoJSON <- function (m, 
+                              latitude, 
+                              longitude, 
+                              popup = NULL,
+                              radius = NULL,
+                              fillColor = NULL,
+                              fillOpacity = NULL,
+                              color = NULL,
+                              opacity = NULL,
+                              weight = NULL,
+                              constantpopup = NULL,
+                              constantradius = NULL,
+                              constantfillColor = NULL,
+                              constantfillOpacity = NULL,
+                              constantcolor = NULL,
+                              constantopacity = NULL,
+                              constantweight = NULL
+) {
+  dataGEO = data.frame(cbind(
+    latitude, 
+    longitude, 
+    popup,
+    radius,
+    fillColor,
+    fillOpacity,
+    color,
+    opacity,
+    weight
+  ), stringsAsFactors=FALSE)
+  names(dataGEO) = c(getLabel(latitude,"latitude"), 
+                     getLabel(longitude,"longitude"),
+                     getLabel(popup,"popup"),
+                     getLabel(radius,"radius"),
+                     getLabel(fillColor,"fillColor"),
+                     getLabel(fillOpacity,"fillOpacity"),
+                     getLabel(color,"color"),
+                     getLabel(opacity,"opacity"),
+                     getLabel(weight,"weight")
+  )
+  dataGEO = lapply(split(dataGEO, rownames(dataGEO)), as.list)
+  
+  if (is.null(popup) && is.null(constantpopup)) clickable = FALSE
+  else clickable = TRUE
+  
+  if ((is.null(weight) || max(weight)==0) && (is.null(constantweight) || constantweight==0)) stroke = FALSE
+  else stroke = TRUE
+  
+  strOnEachFeature = "#! function(feature, layer){layer.bindPopup(feature.properties.popup)} !#"
+  
+  strAtributes = NULL
+  strAtributes = addAttribute(strAtributes, "radius", radius, constantradius)
+  strAtributes = addAttribute(strAtributes, "fillColor", fillColor, constantfillColor)
+  strAtributes = addAttribute(strAtributes, "fillOpacity", fillOpacity, constantfillOpacity)
+  strAtributes = addAttribute(strAtributes, "color", color, constantcolor)
+  strAtributes = addAttribute(strAtributes, "opacity", opacity, constantopacity)
+  strAtributes = addAttribute(strAtributes, "weight", weight, constantweight)
+  if (!clickable) strAtributes = addAttribute(strAtributes, "clickable", NULL, clickable)
+  if (!stroke) strAtributes = addAttribute(strAtributes, "stroke", NULL, stroke)
+  #   print(strAtributes)
+  
+  strPointToLayer = paste0(
+    "#! function(feature, latlng){"
+    ,"          return L.circleMarker(latlng, {"
+    ,strAtributes
+    ,"})"
+    ,"  } !#")
+  
+  if (!is.null(popup) && clickable)
+    m$geoJson(toGeoJSON(dataGEO), 
+              onEachFeature = strOnEachFeature,
+              pointToLayer = strPointToLayer)  
+  else
+    m$geoJson(toGeoJSON(dataGEO), 
+              pointToLayer = strPointToLayer)  
+  
+  return (m)
+}
+
+#' r.map.addLegend
+#' @export
+r.map.addLegend <- function (m, labels, colors, position = 'bottomright') {
+  m$legend(position = position, 
+           colors   =  colors, 
+           labels   =  labels)
+  return (m)
 }
